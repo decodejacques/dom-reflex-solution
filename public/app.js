baa91baf8fe9c30a4a78bc91bfe7eb3a6fea8c5e
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-10-16 10:43:53 -0400

    Question 6

--- public/app.js
+++ public/app.js
@@ -1,34 +1,26 @@
 let body = document.querySelector('body')
 let lost = false
 let won = false
-let clicked = []
+let timesClicked = 0
 
 
-let clickableButton = (text, index) => {
-    clicked.push(false)
-    let theButton = document.createElement('button')
-    theButton.innerText = text
-    theButton.addEventListener('click', () => {
-        if (won || lost) return
-        clicked[index] = true
-        if (clicked.indexOf(false) === -1) {
-            won = true
-            document.getElementById('status').innerText = "you won!"
-        }
-    })
+
+let theButton = document.createElement('button')
+theButton.innerText = "click me"
+theButton.addEventListener('click', () => {
+    if (won || lost) return
+    timesClicked = timesClicked + 1
+    if (timesClicked >= 3) {
+        won = true
+        document.getElementById('status').innerText = "you won!"
+    }
+})
 
 
-    body.appendChild(theButton)
-}
+body.appendChild(theButton)
 
 let genRand = (start, end) => {
-    return start + Math.floor(Math.random() * (end - start))
-}
-
-let randNum = genRand(2, 7)
-
-for (let i = 0; i < randNum; i++) {
-    clickableButton("button " + i, i)
+    return start + (Math.random() * (end - start))
 }
 
 setTimeout(() => {
