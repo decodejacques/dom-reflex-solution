01c0949f799d3fe7a0e379f52b10025f05ce577e
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-10-16 20:35:47 -0400

    Question 9

--- public/app.js
+++ public/app.js
@@ -1,16 +1,37 @@
 let body = document.querySelector('body')
-let lost = false
-let won = false
-let timesClicked = 0
+
 
 let genRand = (start, end) => {
     return start + Math.floor(Math.random() * (end - start))
 }
 
-let clicksNeeded = genRand(3, 6)
 
+
+let addRestartButton = () => {
+    let restartButton = document.createElement('button')
+    restartButton.innerText = 'restart'
+    restartButton.addEventListener('click', () => {
+        startGame()
+    })
+    body.append(restartButton)
+
+}
 
 let startGame = () => {
+    // remove everything from the body
+    body.innerHTML = ""
+
+    // Add the status div (it used to be in the HTML)
+    let status = document.createElement("div")
+    status.id = 'status'
+    body.appendChild(status)
+
+    // These used to be a global variable
+    let clicksNeeded = genRand(3, 6)
+    let timesClicked = 0
+    let lost = false
+    let won = false
+
 
     let theButton = document.createElement('button')
     theButton.innerText = "" + clicksNeeded
@@ -21,6 +42,7 @@ let startGame = () => {
         if (timesClicked >= clicksNeeded) {
             won = true
             document.getElementById('status').innerText = "you won!"
+            addRestartButton()
         }
     })
     body.appendChild(theButton)
@@ -28,6 +50,7 @@ let startGame = () => {
         if (won || lost) return
         lost = true
         document.getElementById('status').innerText = "You lost!"
+        addRestartButton()
     }, genRand(500, 1300))
 }
 
