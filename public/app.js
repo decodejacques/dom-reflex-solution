ea8b2a1a7db731dea99457f6b82659048004b0c3
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-10-16 20:47:38 -0400

    Question 11

--- public/app.js
+++ public/app.js
@@ -27,38 +27,57 @@ let startGame = () => {
     body.appendChild(status)
 
     // These used to be a global variable
-    let clicksNeeded = genRand(3, 6)
-    let timesClicked = 0
+
     let lost = false
     let won = false
 
+    // random number of buttons
+    let numButtons = genRand(2, 5)
+    clicksNeeded = []
+    timesClicked = []
 
-    let theButton = document.createElement('button')
-    // a DOM element needs to have position:absolute to move it anywhere you want
-    theButton.style.position = 'absolute'
-    theButton.innerText = "" + clicksNeeded
-    theButton.addEventListener('click', () => {
-        if (won || lost) return
+    for (let i = 0; i < numButtons; i++) {
+        // We have to keep track of the number of times clicked for each button
+        clicksNeeded.push(genRand(3, 6))
+        timesClicked.push(0)
 
-        // randomly place the DOM element a certain number of pixels from the top and left
+        let theButton = document.createElement('button')
+        // a DOM element needs to have position:absolute to move it anywhere you want
+        theButton.style.position = 'absolute'
         theButton.style.left = genRand(100, 200) + "px"
         theButton.style.top = genRand(100, 200) + "px"
+        theButton.innerText = "" + clicksNeeded[i]
+        theButton.addEventListener('click', () => {
+            if (won || lost) return
 
-        timesClicked = timesClicked + 1
-        theButton.innerText = "" + (clicksNeeded - timesClicked)
-        if (timesClicked >= clicksNeeded) {
-            won = true
-            document.getElementById('status').innerText = "you won!"
-            addRestartButton()
-        }
-    })
-    body.appendChild(theButton)
+            // randomly place the DOM element a certain number of pixels from the top and left
+            theButton.style.left = genRand(100, 200) + "px"
+            theButton.style.top = genRand(100, 200) + "px"
+
+            timesClicked[i] = timesClicked[i] + 1
+            theButton.innerText = "" + (clicksNeeded[i] - timesClicked[i])
+
+            // For the game to be finished, we need that the number of times clicked
+            // is greater or equal to the number of clicks needed
+            let finished = true
+            for (let i = 0; i < timesClicked.length; i++) {
+                if (timesClicked[i] < clicksNeeded[i]) finished = false
+            }
+
+            if (finished) {
+                won = true
+                document.getElementById('status').innerText = "you won!"
+                addRestartButton()
+            }
+        })
+        body.appendChild(theButton)
+    }
     setTimeout(() => {
         if (won || lost) return
         lost = true
         document.getElementById('status').innerText = "You lost!"
         addRestartButton()
-    }, genRand(500, 1300))
+    }, genRand(5000, 13000))
 }
 
 setTimeout(startGame, genRand(1000, 2000))
